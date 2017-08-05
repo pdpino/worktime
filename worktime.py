58195ae91a7198270ca9b40202d37038aaa7bf65
PABLO PINO <pdpino@uc.cl>
2017-08-05 17:22:18 -0400

    change version number, now really deploy v2.2

--- worktime.py
+++ worktime.py
@@ -5,7 +5,7 @@ Measure the time that you work on different subjects."""
 
 __author__ = "pdpino"
 __program__ = "Worktime"
-__version__ = "2.2dev"
+__version__ = "2.2"
 
 import sys
 import argparse
